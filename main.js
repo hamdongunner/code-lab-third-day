71a48adcd2e5b521e68a687971cfe2cac7320bae
hamdon <hamdongunner@gmail.com>
2017-08-03 20:15:25 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+Vue.component('message',{
+    "props":['array'],
+    'template':`<div class="container">
+       <div v-for="(list,index) in array" class="panel panel-default">
+       <div class="panel-heading">message {{index+1}} {{list.username}}</div>
+         <div class="panel-body">
+          {{list.message}}
+            </div>
+          </div>
+          </div>
+          </div>`,
+
+         'data'(){
+        return {
+            "text":""
+        }
+    },
+
+});
+
+
+
+
+
+
+// Vue.component('task-list',{
+//     'template':`<div><task><slot> </slot>codelab</task>
+//                 <task><slot> </slot>codelab2</task></div>`,
+//     'data'(){
+//         // noinspection JSAnnotator
+//         return {
+//             "lists":["home","about","video","contact"]
+//         }
+//     }
+// });
+//
+//
+// Vue.component('task',{
+//     'template':`<div><slot></slot></li></div>`,
+//     'data'(){
+//         // noinspection JSAnnotator
+//     return {
+//             "lists":["home","about","video","contact"]
+//         }
+//     }
+//
+// });
