3aa1b3c71b5fbc4e5be09fa1f8733e75cd867203
hamdon <hamdongunner@gmail.com>
2017-08-04 14:13:05 +0300

    taylor swift

--- main.js
+++ main.js
@@ -1,49 +1,80 @@
 Vue.component('message',{
-    "props":['message'],
+    "props":['album','index','like'],
     'template':`
-      <div>
-       <div  class="panel panel-default">
-       <div class="panel-heading">message {{message.username}}</div>
-         <div class="panel-body">
-          {{message.message}}
-               </div>
-          </div>
-          
-          </div>
-          </div>`,
+      
+       
+    
+ <div style="-moz-box-shadow: black;box-shadow: black;-o-box-shadow: black;text-shadow: black">
+      <div class="thumbnail">
+        <img :src="album.image" class="img-responsive" alt="Responsive image">
+        <div class="caption">
+          <h3 class="text-center">{{album.artist}}</h3>
+          <a :href="album.url"><p class="text-center">{{album.title}}</p></a>
+           <span @click="like += 1" class="glyphicon glyphicon-heart btn" style="background-color: red;color: white;margin-left: 40px;" aria-hidden="true"></span> <span class="badge" style="margin-bottom: 40px;margin-left: -10px;background-color: red;margin-right: 0px" v-if="like != 0">{{like}}</span>
+          <a style="margin-left: 0px;" href="http://facebook.com/profile.php?=73322363"><span class="glyphicon glyphicon-share-alt btn btn-primary" aria-hidden="true"></span></a>
+        </div>
+     </div>
+  </div>
+    
+    
+    
+         `,
 
          'data'(){
+        return {
+        }
+    },
+
+
+});
+
+
+Vue.component('tool-bar',{
+    'template':`
+    
+
+<nav class="navbar navbar navbar-static-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="background:url(2.jpg);background-size: 80% 100%">
+            <ul class="nav navbar-nav">
+                <li class="active"><img src="1.jpg" class="img-responsive" alt="Responsive image" style="width:40px;margin-top:7px;height:40px;"></li>
+                <li class="active"><a style="color: white;text-shadow: 1px 1px 5px black;font-size:170%" href="#">Home <span class="sr-only">(current)</span></a></li>
+                <li><a style="color: white;text-shadow: 1px 1px 5px black;font-size:170%" href="#">About</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li  class="dropdown">
+                    <a style="color: white;text-shadow: 1px 1px 5px black;font-size:170%" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Acount <span class="caret"></span></a>
+                    <ul  class="dropdown-menu">
+                        <li><a href="#">Sign in</a></li>
+                        <li><a href="#">Sign out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+
+
+`,
+
+    'data'(){
         return {
 
         }
     },
 
 });
-
-
-
-
-
-
-// Vue.component('task-list',{
-//     'template':`<div><task><slot> </slot>codelab</task>
-//                 <task><slot> </slot>codelab2</task></div>`,
-//     'data'(){
-//         // noinspection JSAnnotator
-//         return {
-//             "lists":["home","about","video","contact"]
-//         }
-//     }
-// });
-//
-//
-// Vue.component('task',{
-//     'template':`<div><slot></slot></li></div>`,
-//     'data'(){
-//         // noinspection JSAnnotator
-//     return {
-//             "lists":["home","about","video","contact"]
-//         }
-//     }
-//
-// });
