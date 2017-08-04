da8e2fc76917831afa5528f9543dacacb5604da0
hamdon <hamdongunner@gmail.com>
2017-08-04 10:20:17 +0300

    jquery

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 Vue.component('message',{
-    "props":['array'],
+    "props":['message'],
     'template':`
       <div>
-       <div v-show="list.isV" v-for="(list,index) in array" class="panel panel-default">
-       
-       <div class="panel-heading">message {{index+1}} {{list.username}}</div>
+       <div  class="panel panel-default">
+       <div class="panel-heading">message {{message.username}}</div>
          <div class="panel-body">
-          {{list.message}}
-                      <span @click="list.isV = false" class="glyphicon glyphicon-remove pull-right" aria-hidden="true"></span>
-            </div>
+          {{message.message}}
+               </div>
           </div>
           
           </div>
